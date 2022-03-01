e0934bef7f23633c0f3434869920f0d62df5f2f3
Bettycodes <bettygcodes@gmail.com>
2022-03-01 10:23:49 +0300

    moved code
    moved code from code sand box to vs code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
-function App() {
+export default function WeatherApp(props) {
+  let [city, setCity] = useState("");
+  let [temp, setTemp] = useState("");
+  let [humidity, setHumidity] = useState("");
+  let [wind, setWind] = useState("");
+  let [icon, setIcon] = useState("");
+
+  function submitButton(event) {
+    event.preventDefault();
+    let unit = `metric`;
+    let apiKey = `049fb32dcd4a672d3fcbdb2a37413a71`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+    axios.get(url).then(showTemp);
+  }
+
+  function showTemp(response) {
+    setTemp(response.data.main.temp);
+    setWind(response.data.wind.speed);
+    setHumidity(response.data.main.humidity);
+    setIcon(
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`
+    );
+  }
+
+  function City(event) {
+    setCity(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <form className="search" onSubmit={submitButton}>
+        <input type="search" placeholder="Name city" onChange={City} />
+        <input type="submit" value="search" />
+      </form>
+      <ul  className="city" >
+        <li>Temperature:{Math.round(temp)}</li>
+        <li>Humidity:{Math.round(humidity)}%</li>
+        <li>Wind:{Math.round(wind)}km/h</li>
+        <li>
+          <img src={icon} alt="weather icon" />
+        </li>
+      </ul>
     </div>
   );
 }
-
-export default App;
